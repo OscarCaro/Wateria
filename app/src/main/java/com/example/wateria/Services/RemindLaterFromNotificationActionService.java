025d4a313c8c26267c18c567e90aaa206be56f8d
Óscar Caro <oscaro@ucm.es>
2021-02-18 11:39:53 +0100

    New JobScheduler for checkPlantListForNotificationService

--- app/src/main/java/com/example/wateria/Services/RemindLaterFromNotificationActionService.java
+++ app/src/main/java/com/example/wateria/Services/RemindLaterFromNotificationActionService.java
@@ -16,6 +16,7 @@ import android.widget.Toast;
 import com.example.wateria.DataStructures.Settings;
 import com.example.wateria.JobSchedulers.NotificationJobService;
 import com.example.wateria.Notifications.NotificationClass;
+import com.example.wateria.R;
 import com.jakewharton.threetenabp.AndroidThreeTen;
 
 import java.util.Calendar;
@@ -23,14 +24,10 @@ import java.util.Calendar;
 public class RemindLaterFromNotificationActionService extends Service {
 
     private Context appContext;
-    private JobScheduler jobScheduler;
     private static final int JOB_ID = 0;
-
 
     @Override
     public IBinder onBind(Intent intent) {
-//        // TODO: Return the communication channel to the service.
-//        throw new UnsupportedOperationException("Not yet implemented");
         return null;
     }
 
@@ -38,37 +35,36 @@ public class RemindLaterFromNotificationActionService extends Service {
     public void onCreate(){
         super.onCreate();
         appContext = getApplicationContext();
-        AndroidThreeTen.init(appContext);                           // TODO: needed?
+        AndroidThreeTen.init(appContext);
 
         // Remove notification from status bar
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(appContext);
         notificationManager.cancel(NotificationClass.notificationId);
-
-        //android.os.Debug.waitForDebugger();                                 // <--- Todo: comment
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        Calendar calendar = Calendar.getInstance();
-        calendar.add(Calendar.MINUTE, 5);
 
-        jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+        // 1º Get elapse time from memory
+        int hoursToDelay = new Settings(appContext).getNotifRepetInterval();
+
+        JobScheduler jobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
         ComponentName serviceName = new ComponentName(getPackageName(), NotificationJobService.class.getName());
         JobInfo.Builder builder = new JobInfo.Builder(JOB_ID, serviceName)
                 //.setRequiredNetworkType(JobInfo.NETWORK_TYPE_NONE)
                 //.setOverrideDeadline(calendar.getTimeInMillis())
                 //.setRequiresDeviceIdle(false)
                 //.setRequiresCharging(false)
-                .setPersisted(true)
-                .setBackoffCriteria(6000, JobInfo.BACKOFF_POLICY_LINEAR)
-                .setMinimumLatency(1000 * 60);
+                //.setPersisted(true)
+                //.setBackoffCriteria(6000, JobInfo.BACKOFF_POLICY_LINEAR)
+                .setMinimumLatency(hoursToDelay * 60 * 1000);
 
-        JobInfo myJobinfo = builder.build();
-        jobScheduler.schedule(myJobinfo);
-        Toast.makeText(this, "Job Scheduled, job will run when " +
-                "the constraints are met.", Toast.LENGTH_SHORT).show();
+        JobInfo jobInfo = builder.build();
+        jobScheduler.schedule(jobInfo);
 
-
+        String message = getResources().getString(R.string.notif_postpone_toast_message) + " "
+                + hoursToDelay + " " + getResources().getQuantityString(R.plurals.hours, hoursToDelay);
+        Toast.makeText(appContext, message, Toast.LENGTH_LONG).show();
         /*
 
         // 1º Get elapse time from memory

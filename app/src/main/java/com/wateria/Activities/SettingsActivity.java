72bcf8c34c1011573db1fbfad0c2eaaf57ea50a2
Óscar Caro <oscaro@ucm.es>
2021-02-19 09:26:44 +0100

    Change package name: com.example.wateria to com.wateria

---
+++ app/src/main/java/com/wateria/Activities/SettingsActivity.java
@@ -0,0 +1,251 @@
+
+package com.wateria.Activities;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.app.TimePickerDialog;
+import android.content.DialogInterface;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+import com.wateria.DataStructures.PlantList;
+import com.wateria.DataStructures.Settings;
+import com.wateria.JobSchedulers.NotificationJobService;
+import com.wateria.R;
+import com.wateria.Utils.CommunicationKeys;
+
+import biz.kasual.materialnumberpicker.MaterialNumberPicker;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private Settings settings;
+
+    private TextView notifStyleTextView;
+    private String[] stylesStringArray;
+
+    private TextView notifTimingTextView;
+    private TimePickerDialog notifTimingTimePickerDialog;
+
+    private TextView notifPostponeNumberTextView;
+    private TextView notifPostponeHourTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_layout);
+
+        settings = new Settings(this);
+
+        // Prepare Notification Style UI
+        stylesStringArray = getResources().getStringArray(R.array.settings_notif_styles);
+        notifStyleTextView = findViewById(R.id.settings_notif_style_type);
+        notifStyleTextView.setText(stylesStringArray[settings.getNotifStyle().ordinal()]);
+
+        //Prepare Notification Timing UI
+        notifTimingTextView = findViewById(R.id.settings_notif_timing_hour);
+        formatNotifTimingTextView();
+
+        //Prepare Notification Postpone UI
+        notifPostponeNumberTextView = findViewById(R.id.settings_notif_remind_num_hours);
+        notifPostponeHourTextView = findViewById(R.id.settings_notif_remind_text_hours);
+        notifPostponeNumberTextView.setText(String.valueOf(settings.getNotifRepetInterval()));
+        notifPostponeHourTextView.setText(getResources().getQuantityString(R.plurals.hours, settings.getNotifRepetInterval()));
+    }
+
+
+    public void onNotifStyleBoxClick(View v){
+        AlertDialog.Builder mBuilder = new AlertDialog.Builder(SettingsActivity.this);
+        mBuilder.setTitle(getResources().getString(R.string.settings_notif_style_dialog));
+        mBuilder.setSingleChoiceItems(stylesStringArray, settings.getNotifStyle().ordinal(), new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                // Store new value
+                settings.setNotifStyle(Settings.NotificationStyle.values()[i]);
+                // Update visual text
+                notifStyleTextView.setText(stylesStringArray[i]);
+
+                dialogInterface.dismiss();
+            }
+        });
+
+        AlertDialog mDialog = mBuilder.create();
+        mDialog.show();
+    }
+
+    private void formatNotifTimingTextView(){
+        String unpadded = "" + settings.getNotifHour();
+        String result = "00".substring(unpadded.length()) + unpadded;
+        unpadded = "" + settings.getNotifMinute();
+        result = result + ":" + "00".substring(unpadded.length()) + unpadded;
+        notifTimingTextView.setText(result);
+    }
+
+    public void onNotifTimingBoxClick(View v){
+        if (notifTimingTimePickerDialog == null) {
+            notifTimingTimePickerDialog = new TimePickerDialog(SettingsActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker view, int clickedHour, int clickedMinute) {
+                    settings.setNotifHour(clickedHour);
+                    settings.setNotifMinute(clickedMinute);
+                    //Cancel notifJob with previous timing and set the new one
+                    NotificationJobService.cancelScheduledJob(SettingsActivity.this);
+                    NotificationJobService.scheduleNextJob(SettingsActivity.this);
+
+                    formatNotifTimingTextView();
+                }
+            }, settings.getNotifHour(), settings.getNotifMinute(), true);
+        }
+        notifTimingTimePickerDialog.show();
+        Window window = notifTimingTimePickerDialog.getWindow();
+        window.setLayout(FrameLayout.LayoutParams.MATCH_PARENT, FrameLayout.LayoutParams.WRAP_CONTENT);
+    }
+
+    public void onNotifPostponeBoxClick(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogTheme);
+
+        View view = LayoutInflater.from(this).inflate(
+                R.layout.settings_postpone_dialog,
+                (ConstraintLayout) findViewById(R.id.layout_dialog_container)
+        );
+
+        final MaterialNumberPicker numberPicker = view.findViewById(R.id.settings_postpone_numberpicker);
+        numberPicker.setValue(settings.getNotifRepetInterval());
+
+        builder.setView(view);
+        final AlertDialog alertDialog = builder.create();
+
+        view.findViewById(R.id.postpone_accept_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Store new value
+                settings.setNotifRepetInterval(numberPicker.getValue());
+                // Update visual text:
+                notifPostponeNumberTextView.setText(String.valueOf(settings.getNotifRepetInterval()));
+                notifPostponeHourTextView.setText(getResources().getQuantityString(R.plurals.hours, settings.getNotifRepetInterval()));
+                alertDialog.dismiss();
+            }
+        });
+
+        if(alertDialog.getWindow() != null){
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+        alertDialog.show();
+
+
+
+//        final MaterialNumberPicker numberPicker = new MaterialNumberPicker.Builder(SettingsActivity.this)
+//                .minValue(1)
+//                .maxValue(23)
+//                .defaultValue(settings.getNotifRepetInterval())         // to be placed here
+//                .backgroundColor(getResources().getColor(R.color.colorWhite))
+//                .separatorColor(getResources().getColor(R.color.colorPrimaryFaded))
+//                .textColor(getResources().getColor(R.color.colorPrimary))
+//                .textSize(20)
+//                .enableFocusability(false)
+//                .wrapSelectorWheel(true)
+//                .build();
+//        new AlertDialog.Builder(SettingsActivity.this)
+//                .setView(numberPicker)
+//                .setPositiveButton(getString(android.R.string.ok), new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        // Store new value
+//                        settings.setNotifRepetInterval(numberPicker.getValue());
+//                        // Update visual text:
+//                        notifPostponeNumberTextView.setText(String.valueOf(settings.getNotifRepetInterval()));
+//                        notifPostponeHourTextView.setText(getResources().getQuantityString(R.plurals.hours, settings.getNotifRepetInterval()));
+//                    }
+//                })
+//                .show();
+    }
+
+    public void onDeleteBoxClick(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(SettingsActivity.this);
+        builder.setMessage(R.string.settings_delete_all_warning_text)
+                .setTitle(R.string.settings_delete_all_warning_title)
+                .setCancelable(true)
+                .setPositiveButton(R.string.settings_delete_all_warning_yes, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        //Delete all plants
+                        PlantList plantList = PlantList.getInstance(SettingsActivity.this);
+                        plantList.deleteAll(SettingsActivity.this);
+                        setResult(CommunicationKeys.Settings_Main_ResultDeleteAll);
+
+                        dialog.cancel();
+                    }
+                })
+                .setNegativeButton(R.string.settings_delete_all_warning_no, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public void onLicensesBoxClick(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogTheme);
+        View view = LayoutInflater.from(this).inflate(
+                R.layout.settings_licenses_dialog,
+                (ConstraintLayout) findViewById(R.id.layout_dialog_container)
+        );
+
+        //Make the links clickable
+        TextView textView = (TextView) view.findViewById(R.id.settings_license_text);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+
+        builder.setView(view);
+        final AlertDialog alertDialog = builder.create();
+
+        view.findViewById(R.id.license_accept_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+        if(alertDialog.getWindow() != null){
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+
+        alertDialog.show();
+    }
+
+    public void onAboutBoxClick(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this, R.style.AlertDialogTheme);
+        View view = LayoutInflater.from(this).inflate(
+                R.layout.settings_about_dialog,
+                (ConstraintLayout) findViewById(R.id.layout_dialog_container)
+        );
+        builder.setView(view);
+        final AlertDialog alertDialog = builder.create();
+
+        view.findViewById(R.id.about_accept_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alertDialog.dismiss();
+            }
+        });
+
+        if(alertDialog.getWindow() != null){
+            alertDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        }
+        alertDialog.show();
+    }
+
+    public void onHomeButtonClick(View v){
+        finish();   // Carry DeleteAll flag if applicable
+    }
+
+}
---
+++ app/src/main/java/com/wateria/JobSchedulers/RemindLaterNotificationJobService.java
@@ -0,0 +1,43 @@
+package com.wateria.JobSchedulers;
+
+import android.app.job.JobParameters;
+import android.app.job.JobService;
+import android.content.Context;
+
+import com.wateria.DataStructures.Plant;
+import com.wateria.DataStructures.PlantList;
+import com.wateria.Notifications.NotificationClass;
+import com.jakewharton.threetenabp.AndroidThreeTen;
+
+import java.util.ArrayList;
+
+public class RemindLaterNotificationJobService extends JobService {
+
+    public static final int REMIND_LATER_JOB_ID = 1;
+
+    @Override
+    public boolean onStartJob(JobParameters params) {
+        // Create the new notification
+        AndroidThreeTen.init(getApplicationContext());
+
+        Context context = getApplicationContext();
+
+        // Get a sublist filled with the plants that need to be watered (0 days remaining)
+        ArrayList<Plant> zeroDaysList = PlantList.getInstance(this).get0daysRemSublist();
+
+        if (zeroDaysList.size() > 0) {        // There are plants that need to be watered today
+            //Compute notifications
+            NotificationClass.createNotificationChannel(context);
+            NotificationClass.pushNotification(context, zeroDaysList);
+        }
+
+        // No rescheduling
+
+        return false;
+    }
+
+    @Override
+    public boolean onStopJob(JobParameters params) {
+        return true;
+    }
+}

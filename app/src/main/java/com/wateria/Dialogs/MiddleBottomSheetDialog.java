1ccc63d2154bcf1aa29c911a8c5e1480e3f36edb
Óscar Caro <oscaro@ucm.es>
2021-03-07 17:34:05 +0100

    Button for GoogleLens (no explanation dialog yet)

--- app/src/main/java/com/wateria/Dialogs/MiddleBottomSheetDialog.java
+++ app/src/main/java/com/wateria/Dialogs/MiddleBottomSheetDialog.java
@@ -3,6 +3,7 @@ package com.wateria.Dialogs;
 import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
@@ -18,6 +19,8 @@ import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.wateria.R;
 
 public class MiddleBottomSheetDialog extends BottomSheetDialog {
+
+    private static final String GoogleLensPackage = "com.google.ar.lens";
 
     public MiddleBottomSheetDialog(@NonNull final Context context) {
         super(context, R.style.CustomBottomSheetDialogTheme);
@@ -46,25 +49,48 @@ public class MiddleBottomSheetDialog extends BottomSheetDialog {
         findViewById(R.id.main_middle_rate_box).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Uri uri = Uri.parse("market://details?id=" + context.getPackageName());
-                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_MULTIPLE_TASK | Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
-                try{
-                    context.startActivity(intent);
-                }
-                catch (ActivityNotFoundException e1){
-                    uri = Uri.parse("https://play.google.com/store/apps/details?id=" + context.getPackageName());
-                    intent = new Intent(Intent.ACTION_VIEW, uri);
-                    intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_MULTIPLE_TASK | Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
-                    try{
-                        context.startActivity(intent);
-                    }
-                    catch (ActivityNotFoundException e2){
-                        Toast.makeText(context, context.getResources().getString(R.string.main_middle_rate_error_toast), Toast.LENGTH_SHORT).show();
-                    }
-                }
+                openPlayStore(context, context.getPackageName());
+                dismiss();
             }
         });
+
+        findViewById(R.id.main_middle_lens_box).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PackageManager manager = context.getPackageManager();
+                Intent lensIntent = manager.getLaunchIntentForPackage(GoogleLensPackage);
+                if (lensIntent != null) {
+                    lensIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+                    context.startActivity(lensIntent);
+                }
+                else{
+                    // Show dialog explaining that they have to install the app
+                    openPlayStore(context, GoogleLensPackage);
+
+                }
+                dismiss();
+            }
+        });
+    }
+
+    private void openPlayStore(Context context, String packageName){
+        Uri uri = Uri.parse("market://details?id=" + packageName);
+        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_MULTIPLE_TASK | Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+        try{
+            context.startActivity(intent);
+        }
+        catch (ActivityNotFoundException e1){
+            uri = Uri.parse("https://play.google.com/store/apps/details?id=" + packageName);
+            intent = new Intent(Intent.ACTION_VIEW, uri);
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_MULTIPLE_TASK | Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
+            try{
+                context.startActivity(intent);
+            }
+            catch (ActivityNotFoundException e2){
+                Toast.makeText(context, context.getResources().getString(R.string.main_middle_play_store_error_toast), Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 
 

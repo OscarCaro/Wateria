3810332229d8ba65774ff1b02e5e5d01e370f4e2
Óscar Caro <oscaro@ucm.es>
2021-03-10 19:04:46 +0100

    Google Lens isntall dialog

--- app/src/main/java/com/wateria/Dialogs/MiddleBottomSheetDialog.java
+++ app/src/main/java/com/wateria/Dialogs/MiddleBottomSheetDialog.java
@@ -1,22 +1,20 @@
 package com.wateria.Dialogs;
 
-import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
-import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.wateria.R;
+import com.wateria.Utils.OpenPlayStore;
 
 public class MiddleBottomSheetDialog extends BottomSheetDialog {
 
@@ -49,7 +47,7 @@ public class MiddleBottomSheetDialog extends BottomSheetDialog {
         findViewById(R.id.main_middle_rate_box).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                openPlayStore(context, context.getPackageName());
+                OpenPlayStore.open(context, context.getPackageName());
                 dismiss();
             }
         });
@@ -57,6 +55,7 @@ public class MiddleBottomSheetDialog extends BottomSheetDialog {
         findViewById(R.id.main_middle_lens_box).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // Try opening Google Lens app
                 PackageManager manager = context.getPackageManager();
                 Intent lensIntent = manager.getLaunchIntentForPackage(GoogleLensPackage);
                 if (lensIntent != null) {
@@ -65,33 +64,15 @@ public class MiddleBottomSheetDialog extends BottomSheetDialog {
                 }
                 else{
                     // Show dialog explaining that they have to install the app
-                    openPlayStore(context, GoogleLensPackage);
-
+                    final ViewGroup viewGroup = (ViewGroup) ((ViewGroup) findViewById(android.R.id.content)).getChildAt(0);
+                    GoogleLensDialog.showDialog(context, viewGroup);
                 }
                 dismiss();
             }
         });
     }
 
-    private void openPlayStore(Context context, String packageName){
-        Uri uri = Uri.parse("market://details?id=" + packageName);
-        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-        intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_MULTIPLE_TASK | Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
-        try{
-            context.startActivity(intent);
-        }
-        catch (ActivityNotFoundException e1){
-            uri = Uri.parse("https://play.google.com/store/apps/details?id=" + packageName);
-            intent = new Intent(Intent.ACTION_VIEW, uri);
-            intent.addFlags(Intent.FLAG_ACTIVITY_NO_HISTORY | Intent.FLAG_ACTIVITY_MULTIPLE_TASK | Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
-            try{
-                context.startActivity(intent);
-            }
-            catch (ActivityNotFoundException e2){
-                Toast.makeText(context, context.getResources().getString(R.string.main_middle_play_store_error_toast), Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
+
 
 
 }
